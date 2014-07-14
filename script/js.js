78ef57a49b657fc744987e643bad5fa00bf423e2
luxiangrong <luxiangrongwave@gmail.com>
2014-07-14 19:44:41 +0800

    修改tab块

--- script/js.js
+++ script/js.js
@@ -1,13 +1,12 @@
-$(function(){
+$(function() {
 	//--选项卡
 	$('.indexPart1').find('.tab').find('li').find('img:first').show();
 	$('.indexPart1').find('.tab').find('li:first').find('img').hide();
 	$('.indexPart1').find('.tab').find('li:first').find('img:last').show();
 	$('.tabContentDiv').find('.tabContent:first').show();
-	$('.tab').each(function(i){
-		$(this).find('li').each(function(ii){
-			$(this).hover(
-			function(){
+	$('.tab').each(function(i) {
+		$(this).find('li').each(function(ii) {
+			$(this).click(function() {
 				$('.tab').eq(i).find('li').removeClass('liNow');
 				$(this).addClass('liNow');
 				$('.tabContentDiv').eq(i).find('.tabContent').hide();
@@ -16,19 +15,17 @@ $(function(){
 				$('.tab').eq(i).find('li').find('img:first').show();
 				$(this).find('img').hide();
 				$(this).find('img:last').show();
-				},
-			function(){}	
-				)
-			})
-		})
+			});
+		});
+	});
 	//--错层效果(复杂特效)
-	$(window).scroll(function(){
-		$('.headDiv').css('top',-$(window).scrollTop()/2.5);
-		$('.topImg').css('top',100 -($(window).scrollTop()/1.5));
-		$('.nav').css('top',593 - $(window).scrollTop()/1.2);
+	$(window).scroll(function() {
+		$('.headDiv').css('top', -$(window).scrollTop() / 2.5);
+		$('.topImg').css('top', 100 - ($(window).scrollTop() / 1.5));
+		$('.nav').css('top', 593 - $(window).scrollTop() / 1.2);
 		$('.nav').stop();
 		//$('.nav').css('opacity', (1000-$(window).scrollTop())*0.001)
-		
+
 		// var scrollTop = $(window).scrollTop();
 		// $(".headDiv").css('transform','translate3d(0,' + scrollTop/1.5 + 'px,0)');
 		// $('.topImg').css('transform','translate3d(0,' + (scrollTop/2.5) + 'px,0)');
@@ -36,12 +33,14 @@ $(function(){
 		// $(".headDiv").css('-webkit-transform','translate3d(0,' + scrollTop/1.5 + 'px,0)');
 		// $('.topImg').css('-webkit-transform','translate3d(0,' + (scrollTop/2.5) + 'px,0)');
 		// $('.nav').css('-webkit-transform','translate3d(0,' + (scrollTop/8) + 'px,0)');
-		
-		})
-	//--导航	
-	$('.nav').find('a').each(function(i){
-		$(this).click(function(){
-			$('body,html').animate({scrollTop:$('.indexPart1').offset().top},500);
+
+	});
+	//--导航
+	$('.nav').find('a').each(function(i) {
+		$(this).click(function() {
+			$('body,html').animate({
+				scrollTop : $('.indexPart1').offset().top
+			}, 500);
 			$('.tab').find('li').removeClass('liNow');
 			$('.tab').find('li').eq(i).addClass('liNow');
 			$('.tabContentDiv').find('.tabContent').hide();
@@ -50,7 +49,7 @@ $(function(){
 			$('.tab').find('li').find('img:first').show();
 			$('.tab').find('li').eq(i).find('img').hide();
 			$('.tab').find('li').eq(i).find('img:last').show();
-			})
-		})	
-	//	
-	})
+		});
+	});
+	//
+});
